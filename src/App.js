70d3c263c8f3e8791c1b4ccd8c0be7d8cbae18b3
Muhammad Safwan Karim <safwan_karim@diya-tech.com>
2022-12-06 12:50:05 +0500

    Implemented query parameter and response caching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cache from "./cache/Cache";
 import Home from "./components/Home";
 import MovieDetail from "./components/MovieDetail";
 
 function App() {
+  const cache = new Cache();
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home cache={cache} />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </Router>
